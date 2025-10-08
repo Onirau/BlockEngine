8f3db86dd5a3d5256a1e466d3c3383097d13dcb7
Onirau <61100138+Onirau@users.noreply.github.com>
2025-10-08 06:59:18 +0100

    make vector3 safe

--- src/datatypes/Vector3.cpp
+++ src/datatypes/Vector3.cpp
@@ -22,11 +22,8 @@ static int Vector3_new(lua_State* L) {
 }
 
 static int Vector3_tostring(lua_State* L) {
-    if (lua_isnumber(L, 1)) return 1;
-
     Vector3Game* v3 = (Vector3Game*)luaL_checkudata(L, 1, "Vector3Meta");
     lua_pushfstring(L, "%f, %f, %f", v3->x, v3->y, v3->z);
-
     return 1;
 }
 
@@ -83,7 +80,6 @@ static int Vector3_div(lua_State* L) {
         return 0;
     }
 
-
     Vector3Game* result = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
     *result = Vector3Game{v->x / s, v->y / s, v->z / s};
 
@@ -103,37 +99,36 @@ static int Vector3_unm(lua_State* L) {
     return 1;
 }
 
-
 static int Vector3_index(lua_State* L) {
     Vector3Game* v3 = (Vector3Game*)luaL_checkudata(L, 1, "Vector3Meta");
     const char* key = luaL_checkstring(L, 2);
 
-    if (strcmp(key, "X") == 0)
+    if (strcmp(key, "X") == 0) {
         lua_pushnumber(L, v3->x);
-
-    else if (strcmp(key, "Y") == 0)
+    } else if (strcmp(key, "Y") == 0) {
         lua_pushnumber(L, v3->y);
-
-    else if (strcmp(key, "Z") == 0)
+    } else if (strcmp(key, "Z") == 0) {
         lua_pushnumber(L, v3->z);
-
-    else if (strcmp(key, "Magnitude") == 0)
+    } else if (strcmp(key, "Magnitude") == 0) {
         lua_pushnumber(L, v3->magnitude());
-
-    else if (strcmp(key, "Unit") == 0) {
-        Vector3Game v = v3->unit();
+    } else if (strcmp(key, "Unit") == 0) {
+        Vector3Game* result = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
+        *result = v3->unit();
         luaL_getmetatable(L, "Vector3Meta");
         lua_setmetatable(L, -2);
-        lua_pushlightuserdata(L, &v);
     } else {
+        //Try to find method in the metatable
         luaL_getmetatable(L, "Vector3Meta");
-        lua_getfield(L, -1, "__index");
         lua_getfield(L, -1, key);
 
-        if (lua_isnil(L, -1)) {
-            luaL_error(L, "Attempt to access invalid property/method '%s' of Vector3", key);
-            return 0;
+        if (!lua_isnil(L, -1)) {
+            lua_remove(L, -2);//remove metatable, keep the value
+            return 1;
         }
+
+        lua_pop(L, 2);//pop nil and metatable
+        luaL_error(L, "Attempt to access invalid property '%s' of Vector3", key);
+        return 0;
     }
 
     return 1;
@@ -141,22 +136,34 @@ static int Vector3_index(lua_State* L) {
 
 static int Vector3_abs(lua_State* L) {
     Vector3Game* v3 = (Vector3Game*)luaL_checkudata(L, 1, "Vector3Meta");
-    Vector3Game v = v3->abs();
-    lua_pushlightuserdata(L, &v);
+
+    Vector3Game* result = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
+    *result = v3->abs();
+    luaL_getmetatable(L, "Vector3Meta");
+    lua_setmetatable(L, -2);
+
     return 1;
 }
 
 static int Vector3_ceil(lua_State* L) {
     Vector3Game* v3 = (Vector3Game*)luaL_checkudata(L, 1, "Vector3Meta");
-    Vector3Game v = v3->ceil();
-    lua_pushlightuserdata(L, &v);
+
+    Vector3Game* result = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
+    *result = v3->ceil();
+    luaL_getmetatable(L, "Vector3Meta");
+    lua_setmetatable(L, -2);
+
     return 1;
 }
 
 static int Vector3_floor(lua_State* L) {
     Vector3Game* v3 = (Vector3Game*)luaL_checkudata(L, 1, "Vector3Meta");
-    Vector3Game v = v3->floor();
-    lua_pushlightuserdata(L, &v);
+
+    Vector3Game* result = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
+    *result = v3->floor();
+    luaL_getmetatable(L, "Vector3Meta");
+    lua_setmetatable(L, -2);
+
     return 1;
 }
 
@@ -165,7 +172,6 @@ static int Vector3_dot(lua_State* L) {
     Vector3Game* v3b = (Vector3Game*)luaL_checkudata(L, 2, "Vector3Meta");
 
     lua_pushnumber(L, v3a->dot(*v3b));
-
     return 1;
 }
 
@@ -173,8 +179,10 @@ static int Vector3_cross(lua_State* L) {
     Vector3Game* v3a = (Vector3Game*)luaL_checkudata(L, 1, "Vector3Meta");
     Vector3Game* v3b = (Vector3Game*)luaL_checkudata(L, 2, "Vector3Meta");
 
-    Vector3Game v = v3a->cross(*v3b);
-    lua_pushlightuserdata(L, &v);
+    Vector3Game* result = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
+    *result = v3a->cross(*v3b);
+    luaL_getmetatable(L, "Vector3Meta");
+    lua_setmetatable(L, -2);
 
     return 1;
 }
@@ -184,8 +192,10 @@ static int Vector3_lerp(lua_State* L) {
     Vector3Game* v3b = (Vector3Game*)luaL_checkudata(L, 2, "Vector3Meta");
     float alpha = (float)luaL_checknumber(L, 3);
 
-    Vector3Game v = v3a->lerp(*v3b, alpha);
-    lua_pushlightuserdata(L, &v);
+    Vector3Game* result = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
+    *result = v3a->lerp(*v3b, alpha);
+    luaL_getmetatable(L, "Vector3Meta");
+    lua_setmetatable(L, -2);
 
     return 1;
 }
@@ -199,20 +209,14 @@ static int Vector3_fuzzyeq(lua_State* L) {
 }
 
 void Vector3Game_Bind(lua_State* L) {
-    lua_newtable(L);
+    luaL_newmetatable(L, "Vector3Meta");
 
-    auto pushVector3Const = [L](const char* name, const Vector3Game& v) {
-        Vector3Game* ptr = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
-        *ptr = v;
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
 
-        luaL_getmetatable(L, "Vector3Meta");
-        lua_setmetatable(L, -2);
-        lua_setfield(L, -2, name);
-    };
-
-    luaL_newmetatable(L, "Vector3Meta");//Stack: [Vector3, mt]
     lua_pushcfunction(L, Vector3_index, "__index");
     lua_setfield(L, -2, "__index");
+
     lua_pushcfunction(L, Vector3_tostring, "__tostring");
     lua_setfield(L, -2, "__tostring");
     lua_pushcfunction(L, Vector3_add, "__add");
@@ -232,7 +236,6 @@ void Vector3Game_Bind(lua_State* L) {
     lua_setfield(L, -2, "Ceil");
     lua_pushcfunction(L, Vector3_floor, "Floor");
     lua_setfield(L, -2, "Floor");
-
     lua_pushcfunction(L, Vector3_dot, "Dot");
     lua_setfield(L, -2, "Dot");
     lua_pushcfunction(L, Vector3_cross, "Cross");
@@ -241,7 +244,18 @@ void Vector3Game_Bind(lua_State* L) {
     lua_setfield(L, -2, "Lerp");
     lua_pushcfunction(L, Vector3_fuzzyeq, "FuzzyEq");
     lua_setfield(L, -2, "FuzzyEq");
+
     lua_pop(L, 1);
+
+    lua_newtable(L);
+
+    auto pushVector3Const = [L](const char* name, const Vector3Game& v) {
+        Vector3Game* ptr = (Vector3Game*)lua_newuserdata(L, sizeof(Vector3Game));
+        *ptr = v;
+        luaL_getmetatable(L, "Vector3Meta");
+        lua_setmetatable(L, -2);
+        lua_setfield(L, -2, name);
+    };
 
     pushVector3Const("zero", Vector3_zero);
     pushVector3Const("one", Vector3_one);
@@ -251,5 +265,6 @@ void Vector3Game_Bind(lua_State* L) {
 
     lua_pushcfunction(L, Vector3_new, "new");
     lua_setfield(L, -2, "new");
+
     lua_setglobal(L, "Vector3");
 }
