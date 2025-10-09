8112148f3746baf2ca7538a60596547f2c9f42a6
Onirau <61100138+Onirau@users.noreply.github.com>
2025-10-09 22:55:34 +0100

    replace with original, instead of this fork

--- src/core/Config.h
+++ src/core/Config.h
@@ -7,8 +7,8 @@
 #define ENGINE_VERSION_PATCH 0
 #define ENGINE_VERSION_STRING "0.1.0"
 
-#define ENGINE_ORGANIZATION "Onirau"
-#define ENGINE_WEBSITE "https://github.com/Onirau/BlockEngine"
+#define ENGINE_ORGANIZATION "Zalthen-dev"
+#define ENGINE_WEBSITE "https://github.com/Zalthen-dev/BlockEngine"
 
 //Version Helper
 #define ENGINE_VERSION_NUMBER ((ENGINE_VERSION_MAJOR * 10000) + \
